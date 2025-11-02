16b64357dc3d06fd7a177ea3f4db4c96f8e618c7
Rahul Vishwakarma <vishwakarmarahul0421@gmail.com>
2025-11-02 12:05:45 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -58,6 +58,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'static.aniapi.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
