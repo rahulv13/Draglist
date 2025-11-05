f51ae8f85e51b089021cbad9c1e0286e85df193a
Rahul Vishwakarma <vishwakarmarahul0421@gmail.com>
2025-11-05 18:56:54 +0000

    Perfect 👍 I’ve got your **original SVG code** from the DOCX file.
    Here

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,136 +2,94 @@ import React from "react";
 
 export const DraglistLogo = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 1024 1024"
+    xmlns="http://www.w3.org/2000/svg"
     width="100%"
     height="100%"
     preserveAspectRatio="xMidYMid meet"
     {...props}
   >
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M399.217 341.333H449.6V292.115L399.217 256V341.333Z"
       fill="#43A047"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M399.217 440H449.6V390.783L399.217 355.449V440Z"
       fill="#66BB6A"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M399.217 538.667H449.6V489.449L399.217 454.115V538.667Z"
       fill="#81C784"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M399.217 637.333H449.6V588.115L399.217 552.782V637.333Z"
       fill="#A5D6A7"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M399.217 736H449.6V686.783L399.217 651.449V736Z"
       fill="#C8E6C9"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M573.522 736H523.139V686.783L573.522 651.449V736Z"
       fill="#C8E6C9"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M573.522 637.333H523.139V588.115L573.522 552.782V637.333Z"
       fill="#A5D6A7"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M573.522 538.667H523.139V489.449L573.522 454.115V538.667Z"
       fill="#81C784"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M573.522 440H523.139V390.783L573.522 355.449V440Z"
       fill="#66BB6A"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M573.522 341.333H523.139V292.115L573.522 256V341.333Z"
       fill="#43A047"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M500.522 355.449H449.6V306.231L500.522 270.897V355.449Z"
       fill="#1B5E20"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M500.522 454.115H449.6V404.897L500.522 369.564V454.115Z"
       fill="#2E7D32"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M500.522 552.782H449.6V503.564L500.522 468.231V552.782Z"
       fill="#388E3C"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M500.522 651.449H449.6V602.231L500.522 566.897V651.449Z"
       fill="#43A047"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M500.522 750.115H449.6V700.897L500.522 665.564V750.115Z"
       fill="#66BB6A"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M523.139 750.115H573.522V700.897L523.139 665.564V750.115Z"
       fill="#66BB6A"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M523.139 651.449H573.522V602.231L523.139 566.897V651.449Z"
       fill="#43A047"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M523.139 552.782H573.522V503.564L523.139 468.231V552.782Z"
       fill="#388E3C"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M523.139 454.115H573.522V404.897L523.139 369.564V454.115Z"
       fill="#2E7D32"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M523.139 355.449H573.522V306.231L523.139 270.897V355.449Z"
       fill="#1B5E20"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M488.544 288H461.577L488.544 266.051V288Z"
       fill="#1B5E20"
     />
@@ -145,8 +103,6 @@ export const DraglistLogo = (props: React.SVGProps<SVGSVGElement>) => (
       fill="#2E7D32"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M496.333 768H475.06L496.333 750.115V768Z"
       fill="#C8E6C9"
     />
@@ -156,8 +112,6 @@ export const DraglistLogo = (props: React.SVGProps<SVGSVGElement>) => (
     />
     <path d="M475.06 750.115V768L488.544 759.064L475.06 750.115Z" fill="#A5D6A7" />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M496.333 669.333H475.06L496.333 651.449V669.333Z"
       fill="#66BB6A"
     />
@@ -167,8 +121,6 @@ export const DraglistLogo = (props: React.SVGProps<SVGSVGElement>) => (
       fill="#43A047"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M496.333 570.667H475.06L496.333 552.782V570.667Z"
       fill="#388E3C"
     />
@@ -179,8 +131,6 @@ export const DraglistLogo = (props: React.SVGProps<SVGSVGElement>) => (
       fill="#388E3C"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M496.333 472H475.06L496.333 454.115V472Z"
       fill="#2E7D32"
     />
@@ -190,8 +140,6 @@ export const DraglistLogo = (props: React.SVGProps<SVGSVGElement>) => (
       fill="#1B5E20"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M496.333 373.333H475.06L496.333 355.449V373.333Z"
       fill="#1B5E20"
     />
