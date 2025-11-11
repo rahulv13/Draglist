7ea419009c05d2f9fae51ccea7042e5d7f21c61b
Rahul Vishwakarma <vishwakarmarahul0421@gmail.com>
2025-11-11 18:49:06 +0000

    hey i have thought how about we add a image place holder in logo place

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,35 +1,21 @@
 import * as React from "react";
+import Image from "next/image";
 
 export const DraglistLogo: React.FC<React.SVGProps<SVGSVGElement>> = ({
   className,
   style,
   ...props
 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 200 200"
-    preserveAspectRatio="xMidYMid meet"
-    className={className}
-    style={{ width: "100%", height: "100%", ...style }}
-    fill="currentColor"
-    aria-label="Draglist Logo"
-    {...props}
-  >
-    <path
-      d="M 62.5,50
-         L 62.5,150
-         L 87.5,150
-         L 87.5,112.5
-         L 112.5,150
-         L 137.5,150
-         L 100,100
-         L 137.5,50
-         L 112.5,50
-         L 87.5,87.5
-         L 87.5,50
-         Z"
-    />
-  </svg>
+  <div className={className} style={{...style, position: 'relative'}}>
+      <Image
+        src="https://picsum.photos/seed/draglistlogo/100/100"
+        alt="Draglist Logo Placeholder"
+        layout="fill"
+        objectFit="cover"
+        className="rounded-full"
+        data-ai-hint="logo abstract"
+      />
+  </div>
 );
 
 export const Logo = DraglistLogo;
